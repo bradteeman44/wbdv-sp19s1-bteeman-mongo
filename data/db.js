c4d05bda638f045ac446b1e1a4f99d77d7869c29
Brad Teeman <bradteeman@gmail.com>
2019-04-04 14:24:25 -0400

    changed db name for heroku

--- data/db.js
+++ data/db.js
@@ -1,8 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'white-board';
-    var   connectionString =
-        'mongodb://localhost/';
+    var connectionString = 'mongodb://heroku_6v8mkhfv:v119njimliajbr6suurp38jqjr@ds147344.mlab.com:47344/';
     connectionString += databaseName;
     mongoose.connect(connectionString);
 };
