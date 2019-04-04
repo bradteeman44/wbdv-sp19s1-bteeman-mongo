ca0cc808497e7c3166a0816e60c007e7e89310a6
Brad Teeman <bradteeman@gmail.com>
2019-04-04 14:42:34 -0400

    changed db name for heroku

--- data/db.js
+++ data/db.js
@@ -1,7 +1,11 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'heroku_6v8mkhfv';
-    var connectionString = 'mongodb://heroku_6v8mkhfv:v119njimliajbr6suurp38jqjr@ds147344.mlab.com:47344/';
-    connectionString += databaseName;
-    mongoose.connect(connectionString);
+    mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+        if (error) console.error(error);
+        else console.log('mongo connected');
+    });
+    //const databaseName = 'heroku_6v8mkhfv';
+    //var connectionString = 'mongodb://heroku_6v8mkhfv:v119njimliajbr6suurp38jqjr@ds147344.mlab.com:47344/';
+    //connectionString += databaseName;
+    //mongoose.connect(connectionString);
 };
